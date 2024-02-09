6f7f8af9836f3168750602f38ad87ff72a688174
prarthana <211315@sdmcujire>
2024-02-09 16:47:17 +0530

    Added !

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Socio-G</Text>
+      <Text>Welcome to Socio-G!</Text>
       <StatusBar style="auto" />
     </View>
   );
