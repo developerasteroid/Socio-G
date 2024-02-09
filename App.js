b047fc3424434ab08f39ee6d3f875bec0e0d05c7
suman <developerasteroid@gmail.com>
2024-02-09 16:41:13 +0530

    Revert "added welcome message"
    This reverts commit 5efc5cc0024e5c201a169d773110b13ef58cfed2.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Socio-G</Text>
+      <Text>Open up App.js to start working on your app</Text>
       <StatusBar style="auto" />
     </View>
   );
